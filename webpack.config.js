7665201333ecf3622da3af2770b6d5f41718a266
Mike Botsko <botsko@gmail.com>
2016-03-24 16:37:09 -0700

    Provide dist without lodash, when lodash already present

--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,16 @@ var webpack = require('webpack');
 var package = require('./package.json');
 
 // Are we minifying for prod?
-var PROD = process.env.PROD || '';
+var PROD = process.env.PROD || false;
 
 // Which dir are we building to?
 var DIR = process.env.DIR || 'build';
 
+// Should we bundle lodash?
+var BUNDLE = process.env.BUNDLE || false;
+
 // Include DOM package?
-var EXCLUDE_DOM = process.env.EXCLUDE_DOM || '';
+var EXCLUDE_DOM = process.env.EXCLUDE_DOM || false;
 
 var banner = 'Inspire Tree v' + package.version + '\n\
 ' + package.repository + '\n\
@@ -36,28 +39,55 @@ if (PROD) {
     plugins.push(new webpack.optimize.UglifyJsPlugin({ minimize: true }));
 }
 
-function getFileName() {
-    var base = '[name]';
-
-    if (EXCLUDE_DOM) {
-        base += '-core';
-    }
-
-    if (PROD) {
-        base += '.min';
-    }
-
-    base += '.js';
-
-    return base;
+var externals = {};
+if (!BUNDLE) {
+    externals = {
+        'lodash.assign': 'var _.assign',
+        'lodash.clonedeep': 'var _.cloneDeep',
+        'lodash.defaults': 'var _.defaults',
+        'lodash.foreach': 'var _.forEach',
+        'lodash.find': 'var _.find',
+        'lodash.findindex': 'var _.findIndex',
+        'lodash.findlast': 'var _.findLast',
+        'lodash.isarray': 'var _.isArray',
+        'lodash.isempty': 'var _.isEmpty',
+        'lodash.isfunction': 'var _.isFunction',
+        'lodash.isnumber': 'var _.isNumber',
+        'lodash.isobject': 'var _.isObject',
+        'lodash.isregexp': 'var _.isRegexp',
+        'lodash.isstring': 'var _.isString',
+        'lodash.remove': 'var _.remove',
+        'lodash.slice': 'var _.slice',
+        'lodash.sortby': 'var _.sortBy',
+        'lodash.transform': 'var _.transform'
+    };
 }
 
 module.exports = {
     entry: {
         'inspire-tree': './src/tree.js'
     },
+    externals: externals,
     output: {
-        filename: getFileName(),
+        filename: (function() {
+            var base = '[name]';
+
+            if (EXCLUDE_DOM) {
+                base += '-core';
+            }
+
+            if (BUNDLE) {
+                base += '-bundled';
+            }
+
+            if (PROD) {
+                base += '.min';
+            }
+
+            base += '.js';
+
+            return base;
+        }()),
         path: DIR,
         library: 'InspireTree',
         libraryTarget: 'umd'
